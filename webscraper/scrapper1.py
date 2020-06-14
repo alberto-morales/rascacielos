080356deb484b71fdcadf3d195532fe05a08dfb0
Alberto Morales Morales <mail@albertomorales.eu>
2020-06-14 21:37:28 +0000

    switching from round trip to one way

--- webscraper/scrapper1.py
+++ webscraper/scrapper1.py
@@ -8,10 +8,11 @@ def main():
     # code
     driver = webdriver.Firefox()
     driver.implicitly_wait(20)
-    la_URL = "https://www.edreams.es/travel/#results/type=R;dep=2020-08-15;from=MAD;to=LCG;ret=2020-08-15;collectionmethod=false;airlinescodes=false;internalSearch=true"
+    # la_URL = "https://www.edreams.es/travel/#results/type=R;dep=2020-08-15;from=MAD;to=LCG;ret=2020-08-15;collectionmethod=false;airlinescodes=false;internalSearch=true"
+    la_URL = "https://www.edreams.es/travel/#results/type=O;dep=2020-08-15;from=MAD;to=LCG;internalSearch=true"
     driver.delete_all_cookies()
     driver.get(la_URL)
-    sleep(0.5)  # Time in seconds
+    sleep(5.5)  # Time in seconds
     # Selenium hands the page source to parser
     page_source = driver.page_source
     #
