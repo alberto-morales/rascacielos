8524e91905502a6af79f7584abb89f89d237ec47
Alberto Morales <mail@albertomorales.eu>
2020-06-21 11:19:56 +0200

    switching to yaml config file

--- webscraper/task_creator.py
+++ webscraper/task_creator.py
@@ -1,22 +1,28 @@
-from configparser import ConfigParser
-
+import yaml
+import os 
 from datetime import date, timedelta
 
 from pandas import DataFrame
 
-config = ConfigParser()
-config.read('webscrapper.cfg')
-NUMBER_OF_DAYS   = int(config.get('planner','number_of_days'))
-MAX_DAYS_OF_STAY = int(config.get('planner','max_days_of_stay'))
-
 class Planner():
+    def __init__(self, params=None):
+        """The following configuration elements are required:
+
+            planner.number_of_days
+            planner.max_days_of_stay
+        """
+        with open(os.path.dirname(os.path.abspath(__file__)) + '/webscrapper.yaml') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            self._NUMBER_OF_DAYS = config['planner']['number_of_days']
+            self._MAX_DAYS_OF_STAY = config['planner']['max_days_of_stay']
+
 
     def create_tasks(self):
         dates = DataFrame(columns=('start_date', 'end_date'))
         today = date.today()
-        outer_days_list = [today + timedelta(days=d) for d in range(1, NUMBER_OF_DAYS + 1)]
+        outer_days_list = [today + timedelta(days=d) for d in range(1, self._NUMBER_OF_DAYS + 1)]
         for start_date in outer_days_list:
-            inner_days_list = [start_date + timedelta(days=d) for d in range(MAX_DAYS_OF_STAY + 1)]
+            inner_days_list = [start_date + timedelta(days=d) for d in range(self._MAX_DAYS_OF_STAY + 1)]
             for end_date in inner_days_list:
 #               print(f'de {start_date.strftime("%d/%m/%y")} a {end_date.strftime("%d/%m/%y")}')
                 dates.loc[len(dates)]=[start_date.strftime("%d/%m/%y"),end_date.strftime("%d/%m/%y")]
