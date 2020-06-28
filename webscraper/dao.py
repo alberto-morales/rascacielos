dbc77fa7a6d1a17d5b648c9c2d59d96a56e64ba1
Alberto Morales <mail@albertomorales.eu>
2020-06-28 12:36:47 +0200

    persistence finished?

--- webscraper/dao.py
+++ webscraper/dao.py
@@ -13,10 +13,6 @@ key_schema_str = """
    "type": "record",
    "fields" : [
      {
-       "name" : "extraction_date",
-       "type" : "string"
-     },
-     {
        "name" : "flight_date",
        "type" : "string"
      }   
@@ -31,12 +27,41 @@ value_schema_str = """
    "type": "record",
    "fields" : [
      {
+       "name" : "extraction_date",
+       "type" : "string"
+     },     
+     {
        "name" : "price",
        "type" : "string"
-     }
+     },
+     {
+       "name" : "flight_number",
+       "type" : "string"
+     },
+     {
+       "name" : "airline",
+       "type" : "string"
+     },
+     {
+       "name" : "departure_time",
+       "type" : "string"
+     },
+     {
+       "name" : "arrival_time",
+       "type" : "string"
+     },
+     {
+       "name" : "origin_airport",
+       "type" : "string"
+     },
+     {
+       "name" : "destination_airport",
+       "type" : "string"
+     }               
    ]
 }
 """
+
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -45,6 +70,61 @@ def delivery_report(err, msg):
         print('Message delivery failed: {}'.format(err))
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+
+def create_test_df():
+    #
+    res_price = []
+    res_flight_number = []
+    res_airline = []
+    res_departure_time = []
+    res_arrival_time = []
+    res_origin_airport = []              
+    res_destination_airport = []              
+    res_index = []
+    #
+    price = '178.83'
+    flight_number = '0,VY1017,VY1290'
+    airline = 'Vueling'
+    departure_time = '21:15'
+    arrival_time = '21:10 +1 día'
+    origin_airport = 'Adolfo Suárez Madrid - Barajas, Madrid (MAD)'
+    destination_airport = 'La Coruna, La Coruña (LCG)'
+    index = 0
+    #
+    res_price.append(price)
+    res_flight_number.append(flight_number.strip())
+    res_airline.append(airline.strip())
+    res_departure_time.append(departure_time.strip())
+    res_arrival_time.append(arrival_time.strip())
+    res_origin_airport.append(origin_airport.strip())
+    res_destination_airport.append(destination_airport.strip())
+    res_index.append(index)
+    #
+    res_price = { 'price': res_price}
+    res_flight_number = { 'flight_number': res_flight_number}
+    res_airline = { 'airline': res_airline}
+    res_departure_time = { 'departure_time': departure_time}
+    res_arrival_time = { 'arrival_time': arrival_time}
+    res_origin_airport = { 'origin_airport': res_origin_airport}
+    res_destination_airport = { 'destination_airport': res_destination_airport}
+    #
+    df_price = pd.DataFrame(res_price, index = res_index)
+    df_flight_number = pd.DataFrame(res_flight_number, index = res_index)
+    df_airline = pd.DataFrame(res_airline, index = res_index)
+    df_departure_time = pd.DataFrame(res_departure_time, index = res_index)
+    df_arrival_time = pd.DataFrame(res_arrival_time, index = res_index)
+    df_origin_airport = pd.DataFrame(res_origin_airport, index = res_index)
+    df_destination_airport = pd.DataFrame(res_destination_airport, index = res_index)
+    #
+    df = df_price
+    df = df.join(df_flight_number)
+    df = df.join(df_airline)
+    df = df.join(df_departure_time)
+    df = df.join(df_arrival_time)
+    df = df.join(df_origin_airport)
+    df = df.join(df_destination_airport)    
+    return df
 
 class FlightDAO():
     def __init__(self, params=None):
@@ -72,23 +152,34 @@ class FlightDAO():
         print(extraction_date)
         print(flight_date)
         topic_name = origin + '-' + destination + '-json'
-        key = {
-                "extraction_date": extraction_date,
-                "flight_date": flight_date
-              }
-        value = {
-                    "price": "69,69"
-                }      
-        self._avroProducer.produce(topic=topic_name, key=key, value=value)
-        self._avroProducer.flush()     
+        #
+        for i in range(len(df)) : 
+          price = df.loc[i, 'price']
+          flight_number = df.loc[i, 'flight_number']
+          airline = df.loc[i, 'airline']
+          departure_time = df.loc[i, 'departure_time']
+          arrival_time = df.loc[i, 'arrival_time']
+          origin_airport = df.loc[i, 'origin_airport']
+          destination_airport = df.loc[i, 'destination_airport']         
+          #
+          key = {
+                  "flight_date": flight_date
+                }
+          value = {
+                      "extraction_date": extraction_date,
+                      "price": price,
+                      "flight_number": flight_number,
+                      "airline": airline,
+                      "departure_time": departure_time,
+                      "arrival_time": arrival_time,
+                      "origin_airport": origin_airport,
+                      "destination_airport": destination_airport
+                  }      
+          self._avroProducer.produce(topic=topic_name, key=key, value=value)
+          self._avroProducer.flush()     
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
+    df = create_test_df()
     dao = FlightDAO()
-    res_price = []
-    res_index = []    
-    res_price.append('68,86')
-    res_index.append(1)    
-    res_data = {'price' : res_price}    
-    df = pd.DataFrame(res_data, index = res_index)
     dao.persist(df, 'MAD', 'LCG', date.today().strftime("%Y-%m-%d"), (date.today() + timedelta(days=20)).strftime("%Y-%m-%d"))      
