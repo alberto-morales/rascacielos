efc80b411bf77f18658f96012621985d7abb2c0d
Alberto Morales <mail@albertomorales.eu>
2020-10-25 05:25:09 +0100

    automated four routes training

--- training/train.py
+++ training/train.py
@@ -8,8 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 BASE_OUTPUT_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../models"
-DATA_FILE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../data/test_BCN_MAD_processed.csv"
-MODEL_NAME = "BCN-MAD.bin"
+BASE_DATA_FILE_PATH = os.path.dirname(os.path.abspath(__file__)) + "/../data"
 
 def train_model(data, hyperparams):
     model = sm.OLS(data["train"]["y"], data["train"]["X"]).fit()
@@ -17,13 +16,14 @@ def train_model(data, hyperparams):
     print(str(model.summary()))
     return model
 
-def main():
+def main(data_file_name, model_name):
     print("Running train.py")
 
     hyperparams = []
 
+    data_file_path = os.path.join(BASE_DATA_FILE_PATH, data_file_name)
     # Get the dataframe 
-    df = pd.read_csv(DATA_FILE_PATH) 
+    df = pd.read_csv(data_file_path) 
     if (df is not None):
         X = df[["price", "lag"]]  # Feature Matrix
         y = df["minPrice"]  # Target Variable
@@ -41,7 +41,7 @@ def main():
 
     # Save model file to disk
     os.makedirs(BASE_OUTPUT_PATH, exist_ok=True)
-    model_output_path = os.path.join(BASE_OUTPUT_PATH, MODEL_NAME)
+    model_output_path = os.path.join(BASE_OUTPUT_PATH, model_name)
     joblib.dump(value=model, filename=model_output_path)
 
     # x_pred1 = (124.25, 1)
@@ -52,4 +52,7 @@ def main():
     y_pred1 = y_test
 
 if __name__ == '__main__':
-    main()
+    main('test_BCN_MAD_processed.csv', 'BCN-MAD.bin')
+    main('test_MAD_BCN_processed.csv', 'MAD-BCN.bin')
+    main('test_LCG_MAD_processed.csv', 'LCG-MAD.bin')
+    main('test_MAD_LCG_processed.csv', 'MAD-LCG.bin')
